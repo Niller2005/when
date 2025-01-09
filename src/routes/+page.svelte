ca480f76e40c1b009b6e66240ea8b13cdcfc812e
Jacob Nielsen <jnillern@gmail.com>
2025-01-09 19:20:33 +0100

    Marvel Rivals Season 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	const startCountdown = (date: Date) => {
+		let now = $state(new Date().getTime());
+		let targetDate = date.getTime();
+
+		const x = setInterval(() => {
+			now = new Date().getTime();
+		}, 1000);
+
+		const diff = $derived(targetDate - now);
+
+		const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+			.toString()
+			.padStart(2, '0');
+
+		const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+			.toString()
+			.padStart(2, '0');
+
+		const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+			.toString()
+			.padStart(2, '0');
+
+		const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+			.toString()
+			.padStart(2, '0');
+
+		return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+	};
+</script>
+
+<main class="h-screen w-full">
+	<div class="flex h-full items-center justify-center">
+		<div class="flex flex-col items-center justify-center">
+			<h2 class="text-2xl">🦸Marvel Rivals Season 1 🦸</h2>
+			<h1 class="text-3xl tracking-normal">{startCountdown(new Date('2025-01-10T09:00:00Z'))}</h1>
+		</div>
+	</div>
+</main>
